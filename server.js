1c1da1adee2109f514889e2a25ff70dbca1ab671
matiasscs1 <matias.cedeno@udla.edu.ec>
2025-05-07 13:03:23 -0500

    integracion de swager

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ const zonaRoutes = require('./routes/zonaRoutes');
 
 let swaggerDocument;
 
-// Cargar el archivo Swagger
+// Cargar el archivo Swagger correctamente
 try {
-  swaggerDocument = yaml.load(path.join(__dirname, 'docs/swagger.yaml'));
+  const swaggerPath = path.join(__dirname, 'docs/swagger.yaml');
+  swaggerDocument = yaml.load(fs.readFileSync(swaggerPath, 'utf8'));
   console.log('Archivo Swagger cargado exitosamente');
 } catch (e) {
   console.error('Error al cargar el archivo Swagger:', e);
-  process.exit(1); // Detener el servidor si no se puede cargar el archivo Swagger
+  process.exit(1); // Detener el servidor si no se puede cargar Swagger
 }
 
 const app = express();
@@ -22,9 +23,13 @@ const port = 3000;
 
 app.use(express.json());
 
+// Rutas de API
 app.use('/', zonaRoutes);
+
+// Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Documentación Swagger en http://localhost:${port}/api-docs`);
 });
